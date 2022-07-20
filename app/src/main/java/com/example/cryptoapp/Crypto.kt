48bd273c87241c11feb6c1bec9ce91dfd6720bfa
Petrisor Bogdan <bogdan.petrisor@yopeso.com>
2022-07-21 01:32:13 +0300

    Final layout

--- app/src/main/java/com/example/cryptoapp/Crypto.kt
+++ app/src/main/java/com/example/cryptoapp/Crypto.kt
@@ -8,4 +8,8 @@ data class Crypto(
     val is_new: Boolean,
     val is_active: Boolean,
     val type: String
-)
+){
+    override fun toString(): String {
+        return "   $type:   #$rank    $name    $symbol    $is_new    $is_active"
+    }
+}
--- app/src/main/java/com/example/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/example/cryptoapp/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.cryptoapp
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.TextView
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.io.IOException
@@ -13,6 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val cryptoList = getCrypto(this)
+        val textViewBtc = findViewById<TextView>(R.id.textViewBTC)
+        val textViewETH = findViewById<TextView>(R.id.textViewETH)
+        val textViewUSDT = findViewById<TextView>(R.id.tvTether)
+        for(crypto in cryptoList){
+            when(crypto.name){
+                "Bitcoin" -> textViewBtc.text = crypto.toString()
+                "Ethereum" -> textViewETH.text = crypto.toString()
+                "Tether" -> textViewUSDT.text = crypto.toString()
+            }
+        }
 
     }
     fun getCrypto(context: Context): List<Crypto> {
