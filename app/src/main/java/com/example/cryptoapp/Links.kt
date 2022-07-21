972560d0d43ab137168b4e0f3bfcbd02fe766028
Petrisor Bogdan <bogdan.petrisor@yopeso.com>
2022-07-21 13:23:30 +0300

    Create Links Data Class

---
+++ app/src/main/java/com/example/cryptoapp/Links.kt
@@ -0,0 +1,10 @@
+package com.example.cryptoapp
+
+data class Links(
+    val explorer: String = "",
+    val facebook: String = "",
+    val reddit: String = "",
+    val source_code: String = "",
+    val website: String = "",
+    val youtube: String = "",
+    )
