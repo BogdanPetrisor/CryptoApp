d12de769f52303ccd5fb22f4898de1c65304beac
Petrisor Bogdan <bogdan.petrisor@yopeso.com>
2022-07-21 17:41:23 +0300

    Create Title, Description, isActive and Tags TextViews

--- app/src/main/java/com/example/cryptoapp/CoinDetailsActivity.kt
+++ app/src/main/java/com/example/cryptoapp/CoinDetailsActivity.kt
@@ -1,5 +1,7 @@
 package com.example.cryptoapp
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.cryptoapp.databinding.ActivityCoinDetailsBinding
@@ -7,12 +9,19 @@ import com.example.cryptoapp.databinding.ActivityCoinDetailsBinding
 
 class CoinDetailsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCoinDetailsBinding
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            binding = ActivityCoinDetailsBinding.inflate(layoutInflater)
-            setContentView(binding.root)
-            val coinId = intent.getStringExtra(EXTRA_ID)
-            val coin = coinId?.let { FilesUtils.getCoinByID(this, coinId) }
-            binding.textView.text = coin.toString()
+        super.onCreate(savedInstanceState)
+        binding = ActivityCoinDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val coinId = intent.getStringExtra(EXTRA_ID)
+        val coin = coinId?.let { FilesUtils.getCoinByID(this, coinId) }
+        binding.tvTitle.text = "1. ${coin?.name} \n(${coin?.symbol})"
+        when(coin?.isActive){
+            true->binding.tvIsActive.text = "active"
+            false->{binding.tvIsActive.text = "inactive"
+                    binding.tvIsActive.setTextColor(Color.RED)}
+            else -> {}
+        }
     }
 }
