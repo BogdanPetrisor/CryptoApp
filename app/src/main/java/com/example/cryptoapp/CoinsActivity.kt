f558512e4066f80ca22811adbdb4c40790bf69b0
Petrisor Bogdan <bogdan.petrisor@yopeso.com>
2022-07-21 14:41:17 +0300

    Add binding to CoinsActivity

--- app/src/main/java/com/example/cryptoapp/CoinsActivity.kt
+++ app/src/main/java/com/example/cryptoapp/CoinsActivity.kt
@@ -4,28 +4,29 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.example.cryptoapp.databinding.ActivityMainBinding
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.io.IOException
 
 class CoinsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val cryptoList = FilesUtils.getCrypto(this)
-        val textViewBtc = findViewById<TextView>(R.id.textViewBTC)
-        val textViewETH = findViewById<TextView>(R.id.textViewETH)
-        val textViewUSDT = findViewById<TextView>(R.id.tvTether)
+
         for (crypto in cryptoList) {
             when (crypto.name) {
-                "Bitcoin" -> textViewBtc.text = crypto.toString()
-                "Ethereum" -> textViewETH.text = crypto.toString()
-                "Tether" -> textViewUSDT.text = crypto.toString()
+                "Bitcoin" -> binding.textViewBTC.text = crypto.toString()
+                "Ethereum" -> binding.textViewETH.text = crypto.toString()
+                "Tether" -> binding.textViewUSDT.text = crypto.toString()
             }
         }
 
     }
-
 
 
 }
