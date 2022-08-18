41a4373541b94822c36db9099263777b7bda3977
Petrisor Bogdan <bogdan.petrisor@yopeso.com>
2022-08-18 10:46:23 +0300

    Add LoginViewModel

--- app/src/main/java/com/example/cryptoapp/LoginFragment.kt
+++ app/src/main/java/com/example/cryptoapp/LoginFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.replace
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
@@ -17,59 +21,47 @@ import kotlinx.coroutines.launch
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class LoginFragment : Fragment() {
-    private val viewModel by viewModels<LoginViewModel>()
-
-    private var _binding: FragmentLoginBinding? = null
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private val viewModel: LoginViewModel by viewModels()
+    private lateinit var binding: FragmentLoginBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
-        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
         return binding.root
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //viewModel.username.value = ""
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.loginViewModel = viewModel
         binding.loginButton.setOnClickListener {
-            lifecycleScope.launch(Dispatchers.IO) {
-                //TheMovieDBRepository().apply {
-                val nameObserver = Observer<String> { newName ->
-                    // Update the UI, in this case, a TextView.
+            viewModel.doLogin()
+        }
+        viewModel.state.observe(viewLifecycleOwner) { state ->
+            loginStateObserver(state)
 
-                }
-
-                // Observe the LiveData, passing in this activity as the LifecycleOwner and the observer.
-
-//                    val username = binding.textInputEditText.text.toString()
-//                    val password = binding.passwordText.text.toString()
-//                    val credentials =
-//                        CredentialsModel("bogdipetrisor123", "parola123", requestToken().requestToken)
-//                    val login = login(credentials)
-//                    if (login.success) {
-//                        val homeScreenFragment = SecondFragment()
-//                        activity?.supportFragmentManager?.beginTransaction()?.apply {
-//                        }
-//                            ?.replace(
-//                                R.id.fragment_container_view_tag,
-//                                homeScreenFragment,
-//                                "findThisFragment"
-//                            )
-//                            ?.addToBackStack(null)
-//                            ?.commit()
-//                    }
-                //}
-            }
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun loginStateObserver(state: LoginState) {
+        when (state) {
+            is LoginState.Error -> {
+                Toast.makeText(
+                    requireContext(),
+                    state.message,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            LoginState.InProgress -> {
+            }
+            LoginState.Success ->
+                parentFragmentManager.beginTransaction()
+                    .replace<SecondFragment>(R.id.fragment_container_view_tag)
+                    .commit()
+        }
     }
+
 }
--- app/src/main/java/com/example/cryptoapp/viewModels/LoginViewModel.kt
+++ app/src/main/java/com/example/cryptoapp/viewModels/LoginViewModel.kt
@@ -1,19 +1,72 @@
 package com.example.cryptoapp.viewModels
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cryptoapp.CredentialsModel
+import com.example.cryptoapp.LoginState
 import com.example.cryptoapp.TheMovieDBRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
 
-class LoginViewModel(
-    private val loginRepository: TheMovieDBRepository
-) : ViewModel() {
+class LoginViewModel() : ViewModel() {
 
-    private val _username = MutableLiveData<String>()
-    val username: LiveData<String> = _username
+    private val mdbRepo = TheMovieDBRepository()
+    private var job: Job = Job()
 
-    private val _password = MutableLiveData<String>()
-    val password: LiveData<String> = _password
+    val username = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
+
+    private val _state = MutableLiveData<LoginState>()
+    val state: LiveData<LoginState>
+        get() = _state
+
+    fun doLogin() {
+
+        val usernameValue = username.value
+        val passwordValue = password.value
+
+        if (usernameValue.isNullOrBlank()) {
+            _state.value = LoginState.Error("Username blank;")
+            return
+        }
+        if (passwordValue.isNullOrBlank()) {
+            _state.value = LoginState.Error("Password blank;")
+            return
+        }
+
+        //Cancel any previous login attempt
+        job.cancel()
+
+        //Try to login
+        job = viewModelScope.launch(Dispatchers.IO) {
+            try {
+                _state.postValue(LoginState.InProgress)
+
+                //Get new token
+                val token = mdbRepo.requestToken()
+
+                //Login
+                mdbRepo.login(
+                    CredentialsModel(
+                        usernameValue,
+                        passwordValue,
+                        token.requestToken
+                    )
+                )
+
+                _state.postValue(LoginState.Success)
+
+            } catch (e: HttpException) {
+                _state.postValue(LoginState.Error("Incorrect username or password"))
+                Log.e("LoginViewModel: ", e.message.toString())
+            }
+        }
+    }
 
 
 }
